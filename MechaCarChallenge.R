f58d28f8c40571f1045c556e1f1463ff467ba588
Stephen Levy <stephenlevy59@gmail.com>
2023-11-08 12:15:03 -0600

    updated MechaCarChallenge.R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,6 +2,10 @@ library('tidyverse')
 
 MechaCar_mpg_data <- read.csv('MechaCar_mpg.csv') # import dataset
 
+view(MechaCar_mpg_data)
+
+head(MechaCar_mpg_data)
+
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg_data)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg_data))
@@ -11,6 +15,10 @@ demo_table <- read.csv(file='Suspension_Coil.csv', check.names=F,stringsAsFactor
 total_summary <- demo_table %>% summarize(Mean=mean(PSI), Median=median(PSI), variance=sd(PSI)*sd(PSI), SD=sd(PSI), .groups = 'keep') # create summary table
 
 lot_summary <- demo_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), variance=sd(PSI)*sd(PSI), SD=sd(PSI), .groups = 'keep') # create lot table
+
+view(lot_summary)
+
+view(total_summary)
 
 population_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 
